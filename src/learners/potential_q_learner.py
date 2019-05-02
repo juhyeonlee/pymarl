b179d9e751b1300f92d273a1f53d889e5846d79a
Juhyeon Lee <jhlee899@gmail.com>
2019-05-02 15:19:54 +0900

    normalize mean local q val

--- src/learners/potential_q_learner.py
+++ src/learners/potential_q_learner.py
@@ -112,9 +112,12 @@ class PotentialQLearner:
             target_max_qvals = target_mac_out.max(dim=3)[0]
 
         mac_out[avail_actions == 0] = 0
+        mac_out = mac_out / mac_out.sum(dim=-1, keepdim=True)
+        mac_out[avail_actions == 0] = 0
 
         diff_rewards = chosen_action_qvals - th.mean(mac_out[:, :-1], dim=3)
-        print(diff_rewards)
+        print('ch q val', chosen_action_qvals)
+        print('diff reward', diff_rewards)
 
         # Calculate 1-step Q-Learning targets
         targets = diff_rewards + self.args.gamma * (1 - terminated) * target_max_qvals
