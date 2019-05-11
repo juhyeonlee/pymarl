cc6f72e87ea73bbf26416512099a4362c7bb07a7
Juhyeon Lee <jhlee899@gmail.com>
2019-05-12 02:53:21 +0900

    not using diff q value

--- src/parse_results.py
+++ src/parse_results.py
@@ -2,15 +2,24 @@ import json
 import numpy as np
 import matplotlib.pyplot as plt
 
-f = open('../results/sacred/6/info.json')
+f = open('../results/sacred/5/info.json')
+ff = open('../results/sacred/7/info.json')
 data = json.load(f)
-test_won_mean = data['default_g_action_qvals']
-test_won_mean_t = data['default_g_action_qvals_T']
+data_2 = json.load(ff)
+test_won_mean = data['test_battle_won_mean']
+test_won_mean_t = data['test_battle_won_mean_T']
 test_won_mean = np.array(test_won_mean).astype(np.float32)
 test_won_mean_t = np.array(test_won_mean_t).astype(np.int)
+
+test_won_mean_global_only = data_2['test_battle_won_mean']
+test_won_mean_t_global_only  = data_2['test_battle_won_mean_T']
+test_won_mean_global_only = np.array(test_won_mean_global_only).astype(np.float32)
+test_won_mean_t_global_only  = np.array(test_won_mean_t_global_only ).astype(np.int)
 plt.figure(figsize=(10,3))
 plt.grid()
-plt.plot(test_won_mean_t, test_won_mean)
+plt.plot(test_won_mean_t, test_won_mean, label='qmix')
+plt.plot(test_won_mean_t_global_only, test_won_mean_global_only, label='global q only')
+plt.legend()
 plt.xlabel('T')
 plt.ylabel('default_g_action_qvals')
 plt.show()
