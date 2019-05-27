91faf63d9c064bb6700eb019ac03171992f31e0e
Juhyeon Lee <jhlee899@gmail.com>
2019-05-27 11:13:12 +0900

    load error fix

--- src/parse_results.py
+++ src/parse_results.py
@@ -4,22 +4,31 @@ import matplotlib.pyplot as plt
 
 f = open('../results/sacred/5/info.json')
 ff = open('../results/sacred/10/info.json')
+fff = open('../results/sacred/12/info.json')
 data = json.load(f)
 data_2 = json.load(ff)
+data_3 = json.load(fff)
 test_won_mean = data['test_battle_won_mean']
 test_won_mean_t = data['test_battle_won_mean_T']
 test_won_mean = np.array(test_won_mean).astype(np.float32)
 test_won_mean_t = np.array(test_won_mean_t).astype(np.int)
 
-test_won_mean_global_only = data_2['global_q_taken_mean']
-test_won_mean_t_global_only  = data_2['global_q_taken_mean_T']
+test_won_mean_global_only = data_2['test_battle_won_mean']
+test_won_mean_t_global_only  = data_2['test_battle_won_mean_T']
 test_won_mean_global_only = np.array(test_won_mean_global_only).astype(np.float32)
 test_won_mean_t_global_only  = np.array(test_won_mean_t_global_only ).astype(np.int)
-plt.figure(figsize=(10,3))
+
+test_won_mean_global_only_rnn = data_3['test_battle_won_mean']
+test_won_mean_t_global_only_rnn = data_3['test_battle_won_mean_T']
+test_won_mean_global_only_rnn = np.array(test_won_mean_global_only_rnn).astype(np.float32)
+test_won_mean_t_global_only_rnn= np.array(test_won_mean_t_global_only_rnn).astype(np.int)
+plt.figure(figsize=(11,4))
 plt.grid()
-# plt.plot(test_won_mean_t, test_won_mean, label='qmix')
-plt.plot(test_won_mean_t_global_only, test_won_mean_global_only, label='global q only')
+plt.plot(test_won_mean_t, test_won_mean, label='qmix', lw=1.5)
+plt.plot(test_won_mean_t_global_only, test_won_mean_global_only, label='global q only + mlp', lw=1.5)
+plt.plot(test_won_mean_t_global_only_rnn, test_won_mean_global_only_rnn, label='global q only + rnn', lw=1.5)
+
 plt.legend()
 plt.xlabel('T')
-plt.ylabel('global_q_taken_mean')
+plt.ylabel('test_battle_won_mean')
 plt.show()
