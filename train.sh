42bac3a958b512955b4db0923877bf96b5f23cef
juhyeonlee <jhlee899@gmail.com>
2019-06-06 17:19:45 +0900

    train 25 with 1 runner

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #QMIX
-python src/main.py --config=qmix_smac --env-config=sc2 with env_args.map_name=3s5z
+python src/main.py --config=qmix_smac --env-config=sc2 with env_args.map_name=25m
