2b7c415d3cd576ce4f87d7e1f4ce0e85a646721a
Juhyeon Lee <jhlee899@gmail.com>
2019-07-24 15:01:45 +0900

    same mixer weights for wlu

--- src/modules/mixers/qmix.py
+++ src/modules/mixers/qmix.py
@@ -64,19 +64,22 @@ class QMixer(nn.Module):
         agent_qs_wlu_masked = self.wlu_mask.unsqueeze(0) * agent_qs.expand(-1, self.n_agents, -1) # bs * n_agent * n_agent
         agent_qs_wlu_masked = agent_qs_wlu_masked.view(-1, 1, self.n_agents)
 
-        w1_wlu = th.abs(self.hyper_w1_wlu(states))
-        b1_wlu = self.hyper_b1_wlu(states)
-        w1_wlu = w1_wlu.unsqueeze(1).expand(-1, self.n_agents, -1).reshape(-1, self.n_agents, self.embed_dim)
-        b1_wlu = b1_wlu.unsqueeze(1).expand(-1, self.n_agents, -1).reshape(-1, 1, self.embed_dim)
-
-        hidden_wlu = F.elu(th.bmm(agent_qs_wlu_masked, w1_wlu) + b1_wlu)
-
-        w2_wlu = th.abs(self.hyper_w2_wlu(states))
-        w2_wlu = w2_wlu.unsqueeze(1).expand(-1, self.n_agents, -1).reshape(-1, self.embed_dim, 1)
-        v_wlu = self.V_wlu(states).unsqueeze(1).expand(-1, self.n_agents, -1).reshape(-1, 1, 1)
-
-        y_wlu = th.bmm(hidden_wlu, w2_wlu) + v_wlu
-
+        # w1_wlu = th.abs(self.hyper_w1_wlu(states))
+        # b1_wlu = self.hyper_b1_wlu(states)
+        # w1_wlu = w1_wlu.unsqueeze(1).expand(-1, self.n_agents, -1).reshape(-1, self.n_agents, self.embed_dim)
+        # b1_wlu = b1_wlu.unsqueeze(1).expand(-1, self.n_agents, -1).reshape(-1, 1, self.embed_dim)
+        #
+        # hidden_wlu = F.elu(th.bmm(agent_qs_wlu_masked, w1_wlu) + b1_wlu)
+        #
+        # w2_wlu = th.abs(self.hyper_w2_wlu(states))
+        # w2_wlu = w2_wlu.unsqueeze(1).expand(-1, self.n_agents, -1).reshape(-1, self.embed_dim, 1)
+        # v_wlu = self.V_wlu(states).unsqueeze(1).expand(-1, self.n_agents, -1).reshape(-1, 1, 1)
+        #
+        # y_wlu = th.bmm(hidden_wlu, w2_wlu) + v_wlu
+        #
+        # q_wlu = y_wlu.view(bs, -1, self.n_agents)
+        hidden_wlu = F.elu(th.bmm(agent_qs_wlu_masked, w1) + b1)
+        y_wlu = th.bmm(hidden_wlu, w_final) + v
         q_wlu = y_wlu.view(bs, -1, self.n_agents)
 
         return q_tot, q_wlu
