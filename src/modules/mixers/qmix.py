correct serious error... (masking error for q value)

src/modules/mixers/qmix.py
@@ -38,7 +38,7 @@ class QMixer(nn.Module):
         # q_tot - q(-u) = u agent의 q 값
         # loss function 생각 해보기
 
-        self.wlu_mask = th.eye(self.n_agents, device=args.device)
+        self.wlu_mask = 1 - th.eye(self.n_agents, device=args.device)
 
 
     def forward(self, agent_qs, states):
@@ -64,18 +64,18 @@ class QMixer(nn.Module):
         agent_qs_wlu_masked = self.wlu_mask.unsqueeze(0) * agent_qs.expand(-1, self.n_agents, -1) # bs * n_agent * n_agent
         agent_qs_wlu_masked = agent_qs_wlu_masked.view(-1, 1, self.n_agents)
 
-        # w1_wlu = th.abs(self.hyper_w1_wlu(states))
-        # b1_wlu = self.hyper_b1_wlu(states)
-        w1_wlu = w1.expand(-1, self.n_agents, -1).reshape(-1, self.n_agents, self.embed_dim)
-        b1_wlu = b1.expand(-1, self.n_agents, -1).reshape(-1, 1, self.embed_dim)
+        w1_wlu = th.abs(self.hyper_w1_wlu(states))
+        b1_wlu = self.hyper_b1_wlu(states)
+        w1_wlu = w1_wlu.unsqueeze(1).expand(-1, self.n_agents, -1).reshape(-1, self.n_agents, self.embed_dim)
+        b1_wlu = b1_wlu.unsqueeze(1).expand(-1, self.n_agents, -1).reshape(-1, 1, self.embed_dim)
 
         hidden_wlu = F.elu(th.bmm(agent_qs_wlu_masked, w1_wlu) + b1_wlu)
 
-        # w2_wlu = th.abs(self.hyper_w2_wlu(states))
-        w2_wlu = w_final.expand(-1, self.n_agents, -1).reshape(-1, self.embed_dim, 1)
-        v_wlu = v.expand(-1, self.n_agents, -1).reshape(-1, 1, 1)
+        w2_wlu = th.abs(self.hyper_w2_wlu(states))
+        w2_wlu = w2_wlu.unsqueeze(1).expand(-1, self.n_agents, -1).reshape(-1, self.embed_dim, 1)
+        # v_wlu = self.V_wlu(states).unsqueeze(1).expand(-1, self.n_agents, -1).reshape(-1, 1, 1)
 
-        y_wlu = th.bmm(hidden_wlu, w2_wlu) + v_wlu
+        y_wlu = th.bmm(hidden_wlu, w2_wlu)
 
         q_wlu = y_wlu.view(bs, -1, self.n_agents)
 